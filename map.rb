45fae7c59e56e4cee9577e89ecd6719235cd6554
Glenn Y. Rolland <glenux@glenux.net>
2009-06-12 15:49:30 +0000

    sshfs-mapper: improved ruby version a bit.
    git-svn-id: https://websvn.glenux.net/svn/Upoc/sshfs-mapper/trunk@1389 eaee96b3-f302-0410-b096-c6cfd47f7835

--- map.rb
+++ map.rb
@@ -1,12 +1,14 @@
 module SshfsMapper
 	class Map 
-		def initialize 
+		def initialize( map_path )
+			@path = map_path
 			@host = nil
 			@port = 22
 			@user = nil
-
-
 		end
 
+		def parse()
+			puts "Parsing #{@path}"
+		end
 	end
 end
