77adfe9d6fb72bc5eccfedc40e93850d997a5c2b
Glenn Y. Rolland <glenux@gmail.com>
2011-09-04 04:50:38 +0200

    config & constants: Use constants for default locations.

--- qasim/constants.rb
+++ qasim/constants.rb
@@ -1,6 +1,13 @@
 module Qasim
 	APP_ICON_PATH = File.join QASIM_INCLUDE_DIR, "icons"
-	APP_SYSCONFIG_DIR = "/etc/qasim/maps.d"
 	APP_NAME = "Qasim"
 	APP_VERSION = "0.1"
+
+	APP_SYSCONFIG_DIR = "/etc/qasim/maps.d"
+
+	APP_CONFIG_DIR = if ENV['XDG_CONFIG_HOME'] then
+						 File.join ENV['XDG_CONFIG_HOME'], 'qasim'
+					 else
+						 File.join ENV['HOME'], '.config', 'qasim'
+					 end
 end
