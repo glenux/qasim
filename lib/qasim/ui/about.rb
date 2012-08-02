6dd4069abee9feeefeaf0f4c3b042e11aee735e9
Glenn Y. Rolland <glenux@glenux.net>
2012-08-03 00:09:11 +0200

    data: Move all UI text as resource files.

--- lib/qasim/ui/about.rb
+++ lib/qasim/ui/about.rb
@@ -3,22 +3,52 @@ require 'qasim/ui/about_ui'
 
 module Qasim ; module Ui
 	class About < Qt::Dialog
-		def initialize
+		def initialize(parent = nil)
 			super
-			u = Ui_About.new                                                                                                    
-			u.setup_ui(self)
+			@ui = Ui_About.new                                                                                                    
+			@ui.setup_ui(self)
 
 			#FIXME: attach button events to dialog.close
+			#Qt::Object.connect( w, SIGNAL( :clicked ), a, SLOT( :quit ) )
 			
 			# Change title according to current version
 			title_str = "Qasim v%s (%s)" % [ Qasim::APP_VERSION, Qasim::APP_DATE ]
-			u.title_label.text = Qt::Application.translate(
+			@ui.title_label.text = Qt::Application.translate(
 				"About", 
-				"<html><head/><body><p><span style=\" font-size:11pt; font-weight:600;\">#{title_str}" +
-				"</span></p></body></html>", 
+				title_str, 
 				nil, 
 				Qt::Application::UnicodeUTF8
 			)
+
+			# Read Authors
+			file = Qt::File.new(':/qasim/authors') 
+			if file.open(Qt::File::ReadOnly | Qt::File::Text)
+				stream = Qt::TextStream.new( file )
+				@ui.authors_textedit.text = stream.readAll()	
+				file.close
+			else
+				# FIXME handle error on authors reading
+			end
+
+			# Read Thanks
+			file = Qt::File.new(':/qasim/thanks') 
+			if file.open(Qt::File::ReadOnly | Qt::File::Text)
+				stream = Qt::TextStream.new( file )
+				@ui.thanks_textedit.text = stream.readAll()	
+				file.close
+			else
+				# FIXME handle error on thanks reading
+			end
+
+			# Read License
+			file = Qt::File.new(':/qasim/licence-gpl3') 
+			if file.open(Qt::File::ReadOnly | Qt::File::Text)
+				stream = Qt::TextStream.new( file )
+				@ui.license_textedit.text = stream.readAll()	
+				file.close
+			else
+				# FIXME handle error on licence reading
+			end
 
 			#FIXME: set first tab
 		end
