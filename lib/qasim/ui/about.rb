79e4cf6dd12b28d8ea84ee52e1a1103b5633b2a3
Glenn Y. Rolland <glenux@glenux.net>
2012-08-02 11:41:36 +0200

    about: initialize from parent object in constructor.

--- lib/qasim/ui/about.rb
+++ lib/qasim/ui/about.rb
@@ -4,9 +4,12 @@ require 'qasim/ui/about_ui'
 module Qasim ; module Ui
 	class About < Qt::Dialog
 		def initialize
+			super
 			u = Ui_About.new                                                                                                    
 			u.setup_ui(self)
+
+			#FIXME: attach events to close
+			#FIXME: set first tab
 		end
 	end
-
 end ; end
