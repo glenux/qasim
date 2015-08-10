1b18ea6e4a65250e519c17edeff78f0630ada8d5
Glenn Y. Rolland <glenux@glenux.net>
2015-08-10 17:01:51 +0200

    Move version file content within constants.

--- lib/qasim/constants.rb
+++ lib/qasim/constants.rb
@@ -1,4 +1,11 @@
 module Qasim
+	APP_NAME = "Qasim"
+
+	APP_VERSION = "0.1.11"
+
+	APP_DATE = "2014-05-09"
+
+
   APP_DATA_DIR = Pathname.new(File.dirname(__FILE__)).parent.parent.parent + "data"
 
 	APP_ICON_PATH = File.join APP_DATA_DIR, "icons"
@@ -6,8 +13,9 @@ module Qasim
 	APP_SYSCONFIG_DIR = "/etc/qasim/maps.d"
 
 	APP_CONFIG_DIR = if ENV['XDG_CONFIG_HOME'] then
-						 File.join ENV['XDG_CONFIG_HOME'], 'qasim'
-					 else
-						 File.join ENV['HOME'], '.config', 'qasim'
-					 end
+			               File.join ENV['XDG_CONFIG_HOME'], 'qasim'
+		               else
+			               File.join ENV['HOME'], '.config', 'qasim'
+		               end
+
 end
