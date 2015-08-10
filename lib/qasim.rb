283303e45c50ae57ddd235c0f617c2ed13999eee
Glenn Y. Rolland <glenux@glenux.net>
2015-08-10 16:59:47 +0200

    Move CLI code in the lib.

--- lib/qasim.rb
+++ lib/qasim.rb
@@ -1,11 +1,10 @@
 
-
-require 'qasim/version'
 require 'qasim/constants'
 
 module Qasim
 	autoload :Config, 	'qasim/config'
 	autoload :Map, 	'qasim/map'
 	autoload :Ui, 'qasim/ui'
+	autoload :Cli, 'qasim/cli'
 end
 
