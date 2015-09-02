042e3246325f1ccc7b91de1a7729ad9265f8a4fc
Glenn Y. Rolland <glenux@glenux.net>
2015-09-02 16:19:22 +0200

    Clean up config class.

--- lib/qasim/config.rb
+++ lib/qasim/config.rb
@@ -10,16 +10,14 @@ require 'qasim/map/ssh'
 class Qasim::Config
 	attr_reader :mount_dir
   attr_reader :config_dir
+  attr_reader :debug
+  attr_reader :verbose
 
 	def initialize
 		@mount_dir = File.join ENV['HOME'], 'mnt'
 
 		@config_dir = Qasim::APP_CONFIG_DIR
 		@config_file = nil
-		@initialize_enable = false
-		@umount_enable = false
-		@target = nil
-		@verbose_enable = false
 		@debug = false
 		@verbose = false
 	end
